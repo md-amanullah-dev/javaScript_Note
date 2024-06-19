f310cc3e6d3747c8c459660552b390800903b7bb
AMANULLAH023HY <amana170829@gmail.com>
2024-06-20 00:17:08 +0530

    introduction of JS

---
+++ index.js
@@ -0,0 +1,67 @@
+// variable 
+
+// const me sirf ek baar hi value assing kr skte h 
+// dubarah declare nhi kr skte h 
+// const x = 10;
+// console.log(x);
+// x = 20;
+// console.log(x);
+
+// let me ek baar variable declare krne ke baad dobarah value assign kr skte h 
+// let x = 10;
+// console.log(x);
+// x = 20;
+// console.log(x)
+
+// var  me ek baar variable declare krne ke baad dobarah value assign kr skte h 
+// lekin var global varibale ko declare krne ke liye kiya jata 
+// var x = 10;
+// console.log(x);
+// x = 20;
+// console.log(x)
+
+// function name (){
+//     x = 40;
+//     console.log(x);
+// }
+
+
+// typeof ek method jiske jarye idetify krte h ke wo kis type ka h 
+// let x = 10;
+// const name = "aman";
+// const y = 2.4;
+// const z  =true;
+// console.log(typeof x);
+// console.log(typeof name);
+// console.log(typeof y);
+// console.log(typeof z);
+
+// null ek object datatype h 
+// const object = null;
+// console.log(typeof object)
+
+
+// const x = 10;
+// const y = 20;
+// const z = true;
+// console.log(x + y+ z) // 31 true 1 ko return krata h // and false 0 ko return krta h 
+
+
+// // concat 
+// const x = "22"
+// const y = 22
+// const z = "22"
+// console.log(x+y+z);//222222
+
+// const firstName = "adil";
+// const lastName = "rose";
+// console.log(firstName + " " + lastName)// print space bitween two word
+// console.log(`hey my name is ${firstName} ${lastName}`)// unsing backtic define varibale 
+
+
+
+
+
+
+
+
