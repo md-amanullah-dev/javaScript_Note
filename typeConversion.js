d7108953f7f782c76fa13ace9ba1d53b82180156
AMANULLAH023HY <amana170829@gmail.com>
2024-06-20 01:06:51 +0530

    typeConversion and string and method

---
+++ typeConversion.js
@@ -0,0 +1,62 @@
+// const num = "22";
+// console.log(typeof num);// ye string type ka h 
+// console.log(typeof Number(num))// Number method ka use krke number me convert kr diye 
+
+// const num = 22;
+// console.log(typeof num);// ye Number type ka h 
+// console.log(typeof num.toString)// toString method ka use krke string me convert kr diye 
+
+
+// string and uske method
+
+let sentance = "this is Adil Rose"
+console.log(sentance);// print all sentance
+console.log(sentance[0]);// print first character sentance ka 
+console.log(sentance[2]);// print two character sentance ka 
+console.log(sentance[5]);// print five character sentance ka
+
+// console.log(sentance.indexOf("s"));// indexOf kisi bhi cahrecter ka index number batata h 
+// console.log(sentance.lastIndexOf("s"));// lastIndexOf kisi bhi character ka index number batata h 
+// console.log(sentance.charAt(4));// charAt diye gye index ke base pr uska character batat h 
+// console.log(sentance.endWith("rose"));// endWith method ka use kr kisi check krne ke liye kiya jata h true/false ata  
+// console.log(sentance.startWith("s"));// startWith method ka use kr starting  kisi check krne ke liye kiya jata h true/false ata
+
+// console.log(sentance.includes("s"));// includes check krta h ke usme ye majud h ke nhi aur true/false return krta h 
+
+// console.log(sentance.substring(0,5));// substring check krta h ke ye string ek substring h ya nhi // 0 mtlb 1st character and 5 mtlb 5 se pahle wala character 
+
+// console.log(sentance.slice(0,5)); // slice method ka use krke ek string me se substring find krne ke liye  
+// console.log(sentance.split(a)); // split method ka use krke ek string me skip krke break kr deta 
+// name ChannelSplitterNode("a") 
+// n
+// me   
+
+// console.log(sentance.replace("this","they"))// replace method word ko replace krne ke liye kiya jata h 
+
+// console.log(sentance.length)// string ke length ko malum krne ke liye kiya jata h 
+// console.log(sentance.toLowerCase)// string ko lower case me convert krne ke liye kiya jata h
+// console.log(sentance.toUpperCase)// string ko upper case me convert krne ke liye kiya jata h
+
+
+// all string method 
+String length
+String charAt()
+String charCodeAt()
+String at()
+String [ ]
+String slice()
+String substring()
+String substr()
+String toUpperCase()
+String toLowerCase()
+String concat()
+String trim()
+String trimStart()
+String trimEnd()
+String padStart()
+String padEnd()
+String repeat()
+String replace()
+String replaceAll()
+String split()
+
