c05032b5e24098f8b6c75ddd9b511dc348d3cffa
AMANULLAH023HY <amana170829@gmail.com>
2024-06-20 12:02:33 +0530

    array and method

---
+++ array.js
@@ -0,0 +1,38 @@
+// array ek data ka collection hota h 
+
+// const arr = [1,21,13,64,50,16]; // array
+// console.log(arr)// print the array
+// console.log(arr[0]);// specific element ko print kerne ke liye 
+// console.log(arr[1]);// specific element ko print kerne ke liye 
+// console.log(arr[3]);// specific element ko print kerne ke liye 
+
+// const arr = [11,22,"rose",76.3,"adil"];
+
+// const arr = [];
+// arr[0] = 12 // 0 index pr element ko insert krna
+// arr[1] = 15 // 0 index pr element ko insert krna
+// arr[2] = 20 // 0 index pr element ko insert krna
+// // console.log(arr)
+
+
+// array method 
+// const arr = [1,12,23];
+
+// arr.push(7)// arr ke last me element ko insert krta h 
+// arr.pop()// arr ke last element ko delete krta h 
+// arr.unshift(7)// arr ke first me element ko insert krta h 
+// arr.shift()// arr ke first element ko delete krta h 
+// arr.splice(0,2)// arr ke range me element ko delete kr deta h aur ek new arr return krta h(0-start kaha se krna h and 2-kitna elemnt delete krna h )
+// arr.reverse()// arr ke elemnt ko reverse krne ke liye kiya jata h 
+// arr.slice(1,3)// slice ke jarye ek specific range ke elemt ka arr return krta h 
+// arr.includes(3)// ye check krta h ke arr me elemnt mojud h ya nhi true/false return krta h 
+
+// // arr me loop ke help se 100 time elemnt insert krnana
+// const arr = [];
+// for(i =1;i<=100;i++){
+//     arr[i] = i;
+// }
+// console.log(arr);
+
+
+
--- typeConversion.js
+++ typeConversion.js
@@ -39,24 +39,24 @@ console.log(sentance[5]);// print five character sentance ka
 
 
 // all string method 
-String length
-String charAt()
-String charCodeAt()
-String at()
-String [ ]
-String slice()
-String substring()
-String substr()
-String toUpperCase()
-String toLowerCase()
-String concat()
-String trim()
-String trimStart()
-String trimEnd()
-String padStart()
-String padEnd()
-String repeat()
-String replace()
-String replaceAll()
-String split()
+// String length
+// String charAt()
+// String charCodeAt()
+// String at()
+// String [ ]
+// String slice()
+// String substring()
+// String substr()
+// String toUpperCase()
+// String toLowerCase()
+// String concat()
+// String trim()
+// String trimStart()
+// String trimEnd()
+// String padStart()
+// String padEnd()
+// String repeat()
+// String replace()
+// String replaceAll()
+// String split()
 
