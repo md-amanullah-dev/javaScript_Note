a57a6214cc1590f9ff0813a4adc0ae4071a6abf2
AMANULLAH023HY <amana170829@gmail.com>
2024-06-20 12:10:19 +0530

    object introduction

---
+++ object.js
@@ -0,0 +1,21 @@
+// object ek key value pair ka collection hota h 
+// object ko {} se jarye declare kiye jata h 
+
+const  myObj = {
+    name:"Adil",
+    city:"SPJ",
+    phone:9608791598,
+    male:true
+}//object
+console.log(myObj)
+
+// // access the value of Object 
+// console.log(myObj.name)
+// console.log(myObj.city)
+// console.log(myObj.phone)
+
+// data insert in Object 
+// myObj.newData = "diploma"
+// console.log(myObj)//myObj me newData add ho jayega 
+
+
